0c64836dcb79a05e38454f28cc7de07bcb7afc9c
adward27 <whalsrnr2741@gmail.com>
2025-01-16 21:14:37 +0900

    아이디 저장 및 자동 로그인 구현

--- JS/login.js
+++ JS/login.js
@@ -1,29 +1,67 @@
-let login = () =>{
+let login = (event) =>{
     event.preventDefault();
     const userId = document.getElementById("id").value;
     const userPw = document.getElementById("pw").value;
 
-    const id = sessionStorage.getItem("userId");
-    const pw = sessionStorage.getItem("userPw");
+    const id = localStorage.getItem("userId");
+    const pw = localStorage.getItem("userPw");
     
    if(id!=null){
     if(userId===id){
         if(userPw===pw){
             alert("로그인 성공");
+            if(document.getElementById("remember_id").checked){
+                remember_id();
+            }
+            if(document.getElementById("auto_login").checked){
+                remember_id_pw();
+            }
             window.location.href = "main.html";
         }
         else{
             alert("비밀번호를 잘못 입력하셨습니다..");
-            location.reload();
+            clear();
         }
     }
     else{
         alert("id를 잘못 입력하셨습니다..");
-        location.reload();
+        clear();
     }
    }
    else{
     alert("등록되지 않은 id 입니다..");
-    location.reload();
+    clear();
    }
+}
+
+window.onload = function(){
+    const remember_id = localStorage.getItem("remember_id");
+    const auto_login = localStorage.getItem("auto_login");
+
+    if(remember_id){
+        const id = localStorage.getItem("userId");
+        document.getElementById("id").value = id;
+    }
+
+    if(auto_login){
+        const id = localStorage.getItem("userId");
+        const pw = localStorage.getItem("userPw");
+        document.getElementById("id").value = id;
+        document.getElementById("pw").value = pw;
+        login(event);
+    }
+}
+
+function remember_id(){
+    localStorage.setItem("remember_id",true);
+}
+
+function remember_id_pw(){
+    remember_id();
+    localStorage.setItem("auto_login",true);
+}
+
+function clear(){
+    document.getElementById("id").value = "";
+    document.getElementById("pw").value = "";
 }
--- JS/signup.js
+++ JS/signup.js
@@ -6,10 +6,11 @@ function signup(){
     const userPw2 = document.getElementById("pw2").value;
 
     if(userPw1===userPw2){
-        sessionStorage.setItem('name',userName);
-        sessionStorage.setItem('userId',userId);
-        sessionStorage.setItem('userPw',userPw1);
-        // window.location.href = "main.html";
+        localStorage.setItem('name',userName);
+        localStorage.setItem('userId',userId);
+        localStorage.setItem('userPw',userPw1);
+        alert("회원가입이 완료되었습니다. 로그인 페이지로 이동합니다.")
+        window.location.href = "login.html";
     }
     else{
         alert("비밀번호가 일치하지 않습니다..");
