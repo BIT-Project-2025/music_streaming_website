93d427d91b91441565cb60c960efcd1d9aeb48f8
adward27 <whalsrnr2741@gmail.com>
2025-01-17 20:29:02 +0900

    추가 : 로그인 실패시 메세지가 출력됨

--- JS/login.js
+++ JS/login.js
@@ -2,14 +2,17 @@ let login = (event) =>{
     event.preventDefault();
     const userId = document.getElementById("id").value;
     const userPw = document.getElementById("pw").value;
+    const wrongDiv = document.getElementById("wrong_msg");
 
     const id = localStorage.getItem("userId");
     const pw = localStorage.getItem("userPw");
     
    if(id!=null){
-    if(userId===id){
+    if(userPw.length<6){
+        wrongDiv.textContent = ('비밀번호는 6~8자리로 입력해주세요');
+    }
+    else if(userId===id){
         if(userPw===pw){
-            alert("로그인 성공");
             if(document.getElementById("remember_id").checked){
                 remember_id();
             }
@@ -19,17 +22,17 @@ let login = (event) =>{
             window.location.href = "main.html";
         }
         else{
-            alert("비밀번호를 잘못 입력하셨습니다..");
+            wrongDiv.textContent = "비밀번호를 잘못 입력하셨습니다."
             clear();
         }
     }
     else{
-        alert("id를 잘못 입력하셨습니다..");
+        wrongDiv.textContent = "id를 잘못 입력하셨습니다..";
         clear();
     }
    }
    else{
-    alert("등록되지 않은 id 입니다..");
+    wrongDiv.textContent = "등록되지 않은 id 입니다..";
     clear();
    }
 }
@@ -41,13 +44,15 @@ window.onload = function(){
     if(remember_id){
         const id = localStorage.getItem("userId");
         document.getElementById("id").value = id;
-    }
+        document.getElementById("remember_id").checked = true;
+      }
 
     if(auto_login){
         const id = localStorage.getItem("userId");
         const pw = localStorage.getItem("userPw");
         document.getElementById("id").value = id;
         document.getElementById("pw").value = pw;
+        document.getElementById("auto_login").checked = true;
         login(event);
     }
 }
